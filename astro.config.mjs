a868607ae5758b4254a9380ee509bef87f3910e5
AWC Team <webteam@azwestern.edu>
2024-11-14 08:56:57 -0700

    Fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "https://brndnsmth.github.io",
+  site: config.site.base_url
+    ? config.site.base_url
+    : "https://awcteam.github.io",
   base: config.site.base_path ? config.site.base_path : "/western-voice",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {},
